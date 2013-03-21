e8dc944c226facc3ec8e1131b00d304047e93f83
leezhm <leezhm@126.com>
2013-03-22 00:54:14 +0800

    It can rotate the ball now.

--- uniGame02/Assets/Sources/BallMove.cs
+++ uniGame02/Assets/Sources/BallMove.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class BallMove : MonoBehaviour 
 {
+	/// <summary>
+	/// The rotate speed.
+	/// </summary>
+	private int rotateSpeed = 6000;
+	
 	// Use this for initialization
 	void Start () 
 	{
@@ -18,9 +23,10 @@ public class BallMove : MonoBehaviour
 	// Draw GUI
 	void OnGUI()
 	{
-		if(GUI.Button(new Rect(20, 20, 100, 40), new GUIContent("Just Do It!")))
+		if(GUI.Button(new Rect(20, 20, 100, 40), new GUIContent("Just Do it")))
 		{
-			
+			// Transform
+			transform.Rotate(Vector3.up * Time.deltaTime * (-rotateSpeed));
 		}
 	}
 }
