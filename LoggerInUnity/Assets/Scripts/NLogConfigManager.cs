1563a3ba345fef74b264d4110b17495f6450bbc6
leezhm <leezhm@126.com>
2013-04-05 03:36:21 +0800

    add new project LoggerInUnity

---
+++ LoggerInUnity/Assets/Scripts/NLogConfigManager.cs
@@ -0,0 +1,60 @@
+//
+//	This is a singleton class
+//
+
+using System;
+
+
+public sealed class NLogConfigManager
+{
+	
+	#region Singleton 
+	private NLogConfigManager ()
+	{
+	}
+	
+	public static NLogConfigManager Instance
+	{
+		get
+		{
+			return Nested.instance;
+		}
+	}
+	
+	private class Nested
+	{
+		internal readonly static NLogConfigManager instance = new NLogConfigManager();
+	}
+	#endregion // Singleton
+	
+	public void InitNLog()
+	{ 
+		// Step 1. Create configuration object 
+		NLog.Config.LoggingConfiguration gConfig = new NLog.Config.LoggingConfiguration();
+		
+        // Step 2. Create targets and add them to the configuration 
+        NLog.Targets.ColoredConsoleTarget consoleTarget = new NLog.Targets.ColoredConsoleTarget();
+        gConfig.AddTarget("console", consoleTarget); 
+         
+        NLog.Targets.FileTarget fileTarget = new NLog.Targets.FileTarget();
+        gConfig.AddTarget("file", fileTarget); 
+         
+        // Step 3. Set target properties   
+        consoleTarget.Layout = "${date:format=HH\\:MM\\:ss} ${logger} ${message}"; 
+        fileTarget.FileName = UnityEngine.Application.dataPath + "/../../file.txt"; 
+        fileTarget.Layout = "${message}"; 
+		
+		UnityEngine.Debug.Log(fileTarget.FileName);
+         
+        // Step 4. Define rules 
+        NLog.Config.LoggingRule rule1 = new NLog.Config.LoggingRule("*", NLog.LogLevel.Debug, consoleTarget); 
+        gConfig.LoggingRules.Add(rule1); 
+ 
+        NLog.Config.LoggingRule rule2 = new NLog.Config.LoggingRule("*", NLog.LogLevel.Debug, fileTarget); 
+        gConfig.LoggingRules.Add(rule2); 
+         
+        // Step 5. Activate the configuration 
+        NLog.LogManager.Configuration = gConfig;
+	}
+}
+
---
+++ LoggerInUnity/Assets/Scripts/NLogInUnity.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class NLogInUnity : MonoBehaviour 
+{
+	/// <summary>
+	/// NLog In Unity.
+	/// </summary>
+	private static NLog.Logger logger; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		NLogConfigManager.Instance.InitNLog();
+		logger = NLog.LogManager.GetCurrentClassLogger();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		logger.Debug("NLog In Unity : " + Time.time.ToString());
+	}
+}
